79535f2f1e8de17aeaa446a9622924b34bdf33fa
Andrus Adamchik <andrus@objectstyle.com>
2017-11-23 13:48:10 +0300

    new healthcheck API

--- bootique-bom-tests/bootique-test-jdbc/src/test/java/io/bootique/bom/jdbc/DerbyHealthChecksIT.java
+++ bootique-bom-tests/bootique-test-jdbc/src/test/java/io/bootique/bom/jdbc/DerbyHealthChecksIT.java
@@ -3,6 +3,7 @@ package io.bootique.bom.jdbc;
 import io.bootique.BQRuntime;
 import io.bootique.metrics.health.HealthCheckOutcome;
 import io.bootique.metrics.health.HealthCheckRegistry;
+import io.bootique.metrics.health.HealthCheckStatus;
 import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,21 +22,21 @@ public class DerbyHealthChecksIT {
 
         BQRuntime runtime = testFactory.app("--config=classpath:db.yml").autoLoadModules().createRuntime();
 
-        HealthCheckRegistry healthchecks = runtime.getInstance(HealthCheckRegistry.class);
-        Map<String, HealthCheckOutcome> results = healthchecks.runHealthChecks();
+        HealthCheckRegistry healthChecks = runtime.getInstance(HealthCheckRegistry.class);
+        Map<String, HealthCheckOutcome> results = healthChecks.runHealthChecks();
 
         assertEquals(3, results.size());
 
-        HealthCheckOutcome one = results.get("derby1");
+        HealthCheckOutcome one = results.get("bq.jdbc.derby1.canConnect");
         assertNotNull(one);
-        assertTrue(one.isHealthy());
+        assertEquals(HealthCheckStatus.OK, one.getStatus());
 
-        HealthCheckOutcome two = results.get("derby2");
+        HealthCheckOutcome two = results.get("bq.jdbc.derby2.canConnect");
         assertNotNull(two);
-        assertTrue(two.isHealthy());
+        assertEquals(HealthCheckStatus.OK, two.getStatus());
 
-        HealthCheckOutcome three = results.get("derby3");
+        HealthCheckOutcome three = results.get("bq.jdbc.derby3.canConnect");
         assertNotNull(three);
-        assertFalse(three.isHealthy());
+        assertEquals(HealthCheckStatus.CRITICAL, three.getStatus());
     }
 }
