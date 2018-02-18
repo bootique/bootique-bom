8f341c2ddd504e4f058bb1472cf8b3b2b6d386d1
Andrus Adamchik <andrus@objectstyle.com>
2018-02-18 11:40:24 +0300

    removing/replacing references to deprecated API

--- bootique-bom-tests-all/src/test/java/io/bootique/bom/mvc/MustacheAppIT.java
+++ bootique-bom-tests-all/src/test/java/io/bootique/bom/mvc/MustacheAppIT.java
@@ -1,5 +1,11 @@
 package io.bootique.bom.mvc;
 
+import io.bootique.bom.mvc.r1.MustacheResource;
+import io.bootique.jersey.JerseyModule;
+import io.bootique.jetty.JettyModule;
+import io.bootique.mvc.MvcModule;
+import io.bootique.mvc.mustache.MvcMustacheModule;
+import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -7,27 +13,28 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.assertEquals;
 
 public class MustacheAppIT {
 
-	@Rule
-	public MustacheApp app = new MustacheApp();
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
 
-	@Test
-	public void testRun() throws InterruptedException, ExecutionException, TimeoutException {
+    @Test
+    public void testRun() {
 
-		app.app("--config=src/test/resources/io/bootique/bom/mvc/test.yml").start();
+        testFactory.app("--config=src/test/resources/io/bootique/bom/mvc/test.yml", "--server")
+                .modules(JettyModule.class, JerseyModule.class, MvcModule.class, MvcMustacheModule.class)
+                .module(binder -> JerseyModule.extend(binder).addResource(MustacheResource.class))
+                .run();
 
-		WebTarget base = ClientBuilder.newClient().target("http://localhost:8080/");
+        WebTarget base = ClientBuilder.newClient().target("http://localhost:8080/");
 
-		// added as a part of a package
-		Response r1 = base.path("/mustache").request().get();
-		assertEquals(Status.OK.getStatusCode(), r1.getStatus());
-		assertEquals("hi_myname.", r1.readEntity(String.class));
-	}
+        // added as a part of a package
+        Response r1 = base.path("/mustache").request().get();
+        assertEquals(Status.OK.getStatusCode(), r1.getStatus());
+        assertEquals("hi_myname.", r1.readEntity(String.class));
+    }
 
 }
