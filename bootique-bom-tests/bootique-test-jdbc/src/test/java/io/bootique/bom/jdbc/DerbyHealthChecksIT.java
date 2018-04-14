dd56c841106064bbab97732e2763c3a9c2e1daaf
Andrus Adamchik <andrus@objectstyle.com>
2018-04-14 17:09:58 +0300

    bootique-jdbc#82
    * also take into account lazy DS init

--- bootique-bom-tests/bootique-test-jdbc/src/test/java/io/bootique/bom/jdbc/DerbyHealthChecksIT.java
+++ bootique-bom-tests/bootique-test-jdbc/src/test/java/io/bootique/bom/jdbc/DerbyHealthChecksIT.java
@@ -1,6 +1,7 @@
 package io.bootique.bom.jdbc;
 
 import io.bootique.BQRuntime;
+import io.bootique.jdbc.DataSourceFactory;
 import io.bootique.metrics.health.HealthCheckOutcome;
 import io.bootique.metrics.health.HealthCheckRegistry;
 import io.bootique.metrics.health.HealthCheckStatus;
@@ -25,17 +26,36 @@ public class DerbyHealthChecksIT {
         HealthCheckRegistry healthChecks = runtime.getInstance(HealthCheckRegistry.class);
         Map<String, HealthCheckOutcome> results = healthChecks.runHealthChecks();
 
-        assertEquals(3, results.size());
-
+        // check before DataSources are started
         HealthCheckOutcome one = results.get("bq.jdbc.derby1.canConnect");
         assertNotNull(one);
-        assertEquals(HealthCheckStatus.OK, one.getStatus());
+        assertEquals(HealthCheckStatus.UNKNOWN, one.getStatus());
 
         HealthCheckOutcome two = results.get("bq.jdbc.derby2.canConnect");
         assertNotNull(two);
-        assertEquals(HealthCheckStatus.OK, two.getStatus());
+        assertEquals(HealthCheckStatus.UNKNOWN, two.getStatus());
 
         HealthCheckOutcome three = results.get("bq.jdbc.derby3.canConnect");
+        assertNotNull(three);
+        assertEquals(HealthCheckStatus.UNKNOWN, three.getStatus());
+
+        // init DataSources and re-check
+        runtime.getInstance(DataSourceFactory.class).forName("derby1");
+        runtime.getInstance(DataSourceFactory.class).forName("derby2");
+        runtime.getInstance(DataSourceFactory.class).forName("derby3");
+
+
+        results = healthChecks.runHealthChecks();
+
+        one = results.get("bq.jdbc.derby1.canConnect");
+        assertNotNull(one);
+        assertEquals(HealthCheckStatus.OK, one.getStatus());
+
+        two = results.get("bq.jdbc.derby2.canConnect");
+        assertNotNull(two);
+        assertEquals(HealthCheckStatus.OK, two.getStatus());
+
+        three = results.get("bq.jdbc.derby3.canConnect");
         assertNotNull(three);
         assertEquals(HealthCheckStatus.CRITICAL, three.getStatus());
     }
